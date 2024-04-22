d2f844cbfb9b6baf170a982d914ed2578fbcbe5f
alecbarber <alec@context.ai>
2024-04-22 17:38:00 +0100

    dynamic traceable for sdk

--- getcontext/tracing/__init__.py
+++ getcontext/tracing/__init__.py
@@ -1,4 +1,5 @@
-from ._tools import capture_trace
+from ._tools import capture_trace, dynamic_traceable
 from .trace import *
 from langsmith import traceable
+from langsmith.wrappers import wrap_openai
 from getcontext.generated.models import Evaluator
--- getcontext/tracing/_tools.py
+++ getcontext/tracing/_tools.py
@@ -1,8 +1,10 @@
 import inspect
 import os
+from typing import Callable
 
 from langsmith.run_helpers import get_current_run_tree
 from langsmith import traceable
+from langsmith import client as ls_client
 from getcontext.tracing._helpers import modified_environ
 from getcontext.tracing.trace import Trace
 
@@ -45,6 +47,57 @@ def capture_trace(func, *args, **kwargs) -> Trace:
         __user_function_wrapper(func, *args, **kwargs)
 
     return trace
+
+
+def dynamic_traceable(func: Callable,
+                      run_type: ls_client.RUN_TYPE_T = 'chain',
+                      name: str = None) -> Callable:
+    """
+    Dynamically create a traceable function.
+    
+    Useful when you want to dynamically name a trace or you don't have access to the function definition (e.g. openai SDK functions)
+
+    Args:
+        func (Callable): The function to be traced.
+        run_type (ls_client.RUN_TYPE_T, optional): The type of tracing to be applied. Defaults to 'chain'.
+        name (str, optional): The name of the trace. Defaults to function name.
+
+    Returns:
+        Callable: The wrapped function.
+        
+    Example:
+    .. code-block:: python
+        import openai
+        from getcontext.tracing import dynamic_traceable, capture_trace, Evaluator
+        
+        openai_client = openai.Client()
+        openai_chat = dynamic_traceable(
+            openai_client.chat.completions.create,
+            run_type='llm',
+            name='openai_hello_world')
+        
+        trace = capture_trace(
+            openai_chat,
+            messages=[{"role": "user", "content": "Tell me a fun fact about the world."}],
+            model="gpt-3.5-turbo")
+            
+        evaluator = Evaluator('golden_response', {'golden_response': 'Hello, world'})
+
+        trace.add_evaluator('openai_hello_world', evaluator)
+        trace.evaluate()
+    """
+    if not callable(func):
+        raise TypeError("func argument is not callable.")
+    
+    if name is None:
+        name = func.__name__
+
+    @traceable(run_type=run_type, name=name)
+    def wrapper_fn(*args, **kwargs):
+        return func(*args, **kwargs)
+    wrapper_fn.__name__ = name
+
+    return wrapper_fn
 
 
 def __context_enviromental_variables():
